81483730a1dfd44fd9ac22296befafe29b09813e
Nairita Paul <nairitapaul10@gmail.com>
2021-08-04 19:11:59 +0600

    allow user to input location and update board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,29 +1,52 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-    public int[][] gameBoard() {
-        int[][] board = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+    private String[][] gameBoard = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+    private String changeInput = "O";
+    Scanner sc = new Scanner(System.in);
 
+    public void printBoard(String[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++)
                 System.out.print(board[i][j] + " ");
             System.out.println();
         }
-        return board;
     }
 
-    public int input() {
-        System.out.println("Do you want to play Tic Tac Toe? If yes then play your first ball");
-        Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
-        scanner.close();
-        return input;
+    public void changeBoard(String input) {
+        for (int i = 0; i < this.gameBoard.length; i++) {
+            for (int j = 0; j < this.gameBoard[i].length; j++)
+                if (input.equals(this.gameBoard[i][j])) {
+                    this.gameBoard[i][j] = this.changeInput;
+                    this.toggleChangeInput();
+                }
+        }
+    }
 
+    public void toggleChangeInput() {
+        if (this.changeInput == "O")
+            changeInput = "X";
+        else
+            changeInput = "O";
+    }
+
+    public void play() {
+        String input = sc.next();
+        this.changeBoard(input);
+        this.printBoard(this.gameBoard);
+        System.out.println("Please play your next step");
+    }
+
+    public void cleanup() {
+        this.sc.close();
     }
 
     public static void main(String[] args) {
         TicTacToe tictactoe = new TicTacToe();
-        tictactoe.gameBoard();
-        tictactoe.input();
+        System.out.println("Do you want to play Tic Tac Toe? If yes then play your first step");
+        tictactoe.printBoard(tictactoe.gameBoard);
+        while (true) {
+            tictactoe.play();
+        }
     }
 }
